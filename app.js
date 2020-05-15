21ff5d06e4bb68b3b57219ec9089f201604347cc
Mr4leksandr <aleksandrklemets15@gmail.com>
2020-05-15 16:59:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+function vastus1(){
+    document.getElementById("vastus_1").innerText = "Tarnime tarkvara nii tihti kui võimalik, soovitavalt iga paari nädala kuni paari kuu tagant.";
+}
+
+function vastus2(){
+    document.getElementById("vastus_2").innerText = "Valdkonna spetsialistid ja tarkvaraarendajad peavad töötama igapäevaselt koos kogu projekti vältel.";
+}
+
+function vastus3(){
+    document.getElementById("vastus_3").innerText = "Kõige tõhusam ja tulemuslikum viis info jagamiseks arendusmeeskonnas on näost näkku vestlus.";
+}
+
+function vastus4(){
+    document.getElementById("vastus_4").innerText = "Edu peamiseks mõõdupuuks on töötav tarkvara.";
+}
+
+function vastus5(){
+    document.getElementById("vastus_5").innerText = "Agiilse tarkvaraarenduse protsessid soodustavad jätkusuutlikku arendust. See tähendab, et projektiga saab samas tempos jätkata määramata aja jooksul.";
+}
+
+function vastus6(){
+    document.getElementById("vastus_6").innerText = "Meeskond otsib regulaarselt võimalusi saamaks veelgi tõhusamaks ja muudab end vastavalt vajadusele.";
+}
+
+function vastus7(){
+    document.getElementById("vastus_7").innerText = "Projekti edukuse aluseks on motiveeritud inimesed. Loo neile meeldiv ja toetav töökeskkond ning nad saavad iseseisvalt tööga hakkama.";
+}
+
+function vastus8(){
+    document.getElementById("vastus_8").innerText = "Kõige olulisem on tagada kliendi rahulolu, tarnides talle vajalikku tarkvara võimalikult kiiresti ja tihti.";
+}
+
+function vastus9(){
+    document.getElementById("vastus_9").innerText = "Lihtsus - ebavajaliku töö tegematajätmise kunst - on väga oluline.";
+}
+
+function vastus10(){
+    document.getElementById("vastus_10").innerText = "Parimad arhitektuurilised lahendused, nõuded ja disaintekivad iseorganiseeruvates meeskondades.";
+}
